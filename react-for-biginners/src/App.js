756ac4b08290c7c4701b170017d7ae98d837b3ee
lss7612 <lss76121@naver.com>
2022-02-18 11:52:53 +0900

    0218

---
+++ react-for-biginners/src/App.js
@@ -0,0 +1,19 @@
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+  return (
+    <div>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
