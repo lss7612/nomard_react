931fe6826cf53ba2bd0dbd30b2d29ec38a035845
lss7612 <lss76121@naver.com>
2022-02-25 22:25:11 +0900

    0225

--- coin-tracker-challenge/coin-tracker-challenge/src/App.js
+++ coin-tracker-challenge/coin-tracker-challenge/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+let selected_coin = undefined;
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollor, setDollor] = useState(0);
+  const [coin, setCoin] = useState({});
+
+  const onChange = (e) => {
+    setDollor(e.target.value);
+  };
+
+  const onSelect = (e) => {
+    console.log(e.target);
+    console.log(e.target.value);
+
+    for (let i = 0; i < coins.length; i++) {
+      let coin = coins[i];
+      if (coin.name == e.target.value) {
+        setCoin(coin);
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setLoading(false);
+        setCoins(json);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Coin Tracker Challenge (
+        {coins.length === 0 ? <span>loading...</span> : coins.length})
+      </h1>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <div>
+          <input
+            id="selected_coin"
+            list="coin"
+            placeholder="select coin"
+            onChange={onSelect}
+          ></input>
+          <input
+            type="number"
+            placeholder="how much dollor"
+            vlaue={dollor}
+            onChange={onChange}
+          ></input>
+          <hr />
+          <strong>you can buy {dollor}</strong>
+        </div>
+      )}
+
+      <datalist id="coin">
+        {coins.map((coin, idx) => {
+          return <option key={idx}>{coin.name}</option>;
+        })}
+      </datalist>
     </div>
   );
 }
