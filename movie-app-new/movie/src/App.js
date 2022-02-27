1d944f77f26e4b376a4e9174f2956ddb84520947
lss7612 <lss76121@naver.com>
2022-02-27 17:08:01 +0900

    router

---
+++ movie-app-new/movie/src/App.js
@@ -0,0 +1,16 @@
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
